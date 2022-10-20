00a942a7b450e762b8c3f0037959da2848ddb5ad
NoOneHardy <silas.hardegger@outlook.com>
2022-10-20 16:29:29 +0200

    Startpositionen der Figuren

--- Dokumentation/onlinegames/chess/chess.js
+++ Dokumentation/onlinegames/chess/chess.js
@@ -3,9 +3,12 @@ var col = 0
 var row = 0
 var boardContent = "<table  class='chessboard'>"
 var fieldId = 0
+var blackTurn = false;
+var whiteTurn = true;
 
 //Important constants
 const chessboard = document.getElementById("chessboard")
+const startButton = document.getElementById("startButton")
 const occupancy = []
 
 //Important objects
@@ -18,6 +21,7 @@ function CreateChessboard() {
     while (row < 8) {
         boardContent += "<tr>"
         while (col < 8) {
+            occupancy[fieldId] = false
             boardContent += "<td id='" + fieldId++ + "'></td>"
             col++
         }
@@ -27,10 +31,12 @@ function CreateChessboard() {
     }
     boardContent += "</table>"
     chessboard.innerHTML = boardContent
+
+    startButton.style="display:inline"
 }
 
 //Figuren aufstellen
-function StartingPositionPawns() {
+function StartingPosition() {
     fieldId = 0
     while (fieldId <= 63) {
         for (let x = 0; x < 8;) {
@@ -40,17 +46,65 @@ function StartingPositionPawns() {
                 occupancy[fieldId] = true
             }
 
+            if (activeField.id == 0 || activeField.id == 7) {
+                activeField.innerHTML = "<img class='piece' src='Icons/black_rook.png'>"
+                occupancy[fieldId] = true
+            }
+
+            if (activeField.id == 1 || activeField.id == 6) {
+                activeField.innerHTML = "<img class='piece' src='Icons/black_knight.png'>"
+                occupancy[fieldId] = true
+            }
+
+            if (activeField.id == 2 || activeField.id == 5) {
+                activeField.innerHTML = "<img class='piece' src='Icons/black_bishop.png'>"
+                occupancy[fieldId] = true
+            }
+
+            if (activeField.id == 3) {
+                activeField.innerHTML = "<img class='piece' src='Icons/black_queen.png'>"
+                occupancy[fieldId] = true
+            }
+
+            if (activeField.id == 4) {
+                activeField.innerHTML = "<img class='piece' src='Icons/black_king.png'>"
+                occupancy[fieldId] = true
+            }
+
             if (activeField.id <= 55 && activeField.id >= 48) {
                 activeField.innerHTML = "<img class='piece' src='Icons/white_pawn.png'>"
                 occupancy[fieldId] = true
             }
 
+            if (activeField.id == 56 || activeField.id == 63) {
+                activeField.innerHTML = "<img class='piece' src='Icons/white_rook.png'>"
+                occupancy[fieldId] = true
+            }
+
+            if (activeField.id == 57 || activeField.id == 62) {
+                activeField.innerHTML = "<img class='piece' src='Icons/white_knight.png'>"
+                occupancy[fieldId] = true
+            }
+
+            if (activeField.id == 58 || activeField.id == 61) {
+                activeField.innerHTML = "<img class='piece' src='Icons/white_bishop.png'>"
+                occupancy[fieldId] = true
+            }
+
+            if (activeField.id == 59) {
+                activeField.innerHTML = "<img class='piece' src='Icons/white_queen.png'>"
+                occupancy[fieldId] = true
+            }
+
+            if (activeField.id == 60) {
+                activeField.innerHTML = "<img class='piece' src='Icons/white_king.png'>"
+                occupancy[fieldId] = true
+            }
             fieldId++
             x++
         }
     }
+    
+    startButton.style="display:none"
 }
-
-
 CreateChessboard()
-StartingPositionPawns()
