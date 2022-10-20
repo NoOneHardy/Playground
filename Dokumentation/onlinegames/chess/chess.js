394f7daa708d10d2bb9b02cfbe0e91e703f2ca48
NoOneHardy <silas.hardegger@outlook.com>
2022-10-20 15:52:38 +0200

    Wiederherstellung des Homebuttons und Funktion für das Aufstellen der Bauern

--- Dokumentation/onlinegames/chess/chess.js
+++ Dokumentation/onlinegames/chess/chess.js
@@ -1,17 +1,20 @@
 // Important variables
-var col = 0;
-var row = 0;
+var col = 0
+var row = 0
 var boardContent = "<table  class='chessboard'>"
-var fieldId = 00;
+var fieldId = 0
 
 //Important constants
 const chessboard = document.getElementById("chessboard")
+const occupancy = []
 
 //Important objects
-const WhitePawn1 = {activeCol: 2, activeRow: 1}
+const WhitePawn1 = { field: 01 }
+const WhitePawn2 = { field: 11 }
 
 //Create the chessboard
 function CreateChessboard() {
+    fieldId = 0
     while (row < 8) {
         boardContent += "<tr>"
         while (col < 8) {
@@ -20,11 +23,34 @@ function CreateChessboard() {
         }
         boardContent += "</tr>"
         row++
-        fieldId += 3 
         col = 0
     }
     boardContent += "</table>"
     chessboard.innerHTML = boardContent
 }
 
+//Figuren aufstellen
+function StartingPositionPawns() {
+    fieldId = 0
+    while (fieldId <= 63) {
+        for (let x = 0; x < 8;) {
+            var activeField = document.getElementById(fieldId)
+            if (activeField.id <= 15 && activeField.id >= 8) {
+                activeField.innerHTML = "<img class='piece' src='Icons/black_pawn.png'>"
+                occupancy[fieldId] = true
+            }
+
+            if (activeField.id <= 55 && activeField.id >= 48) {
+                activeField.innerHTML = "<img class='piece' src='Icons/white_pawn.png'>"
+                occupancy[fieldId] = true
+            }
+
+            fieldId++
+            x++
+        }
+    }
+}
+
+
 CreateChessboard()
+StartingPositionPawns()
